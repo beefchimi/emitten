965a0907b89147555ba8d3327987cdea3d6703fc
beefchimi <curtdulmage@gmail.com>
2023-01-25 17:33:46 -0500

    :alembic: [Emitten] Experiment with reusing the declare type

--- src/Emitten.ts
+++ src/Emitten.ts
@@ -1,6 +1,12 @@
 import {EmittenProtected} from './EmittenProtected';
 
 export class Emitten<T> extends EmittenProtected<T> {
+  readonly PublicFn = this.EventFn;
+
+  public off: typeof this.PublicFn = (eventName, listener) => {
+    super.off(eventName, listener);
+  };
+
   public emit<TKey extends keyof T>(eventName: TKey, value?: T[TKey]) {
     super.emit(eventName, value);
   }
