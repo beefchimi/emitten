61bf912f5d33e5fb4fe92e09607a25b40c0d50e8
beefchimi <curtdulmage@gmail.com>
2023-01-30 15:23:10 -0500

    :bug: [Emitten] Fix event name type safety

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 export type EmittenKey = string | symbol;
-export type EmittenListener<V = unknown[]> = (...values: V[]) => void;
+export type EmittenListener<V extends readonly unknown[] = any[]> = (
+  ...values: V
+) => void;
+
 export type EmittenMap = Record<EmittenKey, EmittenListener>;
 
 export type EmittenLibrary<T> = Record<keyof T, Set<T[keyof T]>>;
