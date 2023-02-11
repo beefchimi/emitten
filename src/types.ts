0fe0485a869acce62c4d92cf537deea4bd524740
beefchimi <curtdulmage@gmail.com>
2023-02-11 13:07:37 -0500

    :recycle: [Types] Convert to interface

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,8 @@ export type EmittenListener<V extends readonly unknown[] = any[]> = (
   ...values: V
 ) => void;
 
-export type EmittenMap = Record<EmittenKey, EmittenListener>;
+export interface EmittenMap {
+  [key: EmittenKey]: EmittenListener;
+}
+
 export type EmittenLibrary<T> = Map<keyof T, Set<T[keyof T]>>;
